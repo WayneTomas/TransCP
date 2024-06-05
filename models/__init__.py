51d58046c4a7ab0a04e9bda1b3af8a835bef1957
Wayne Tomas <1170883362@qq.com>
2024-06-05 10:59:46 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,3 @@
-# from .VLTVG import build_vgmodel
-# from .VLTVG_VD import build_vgmodel
-# from .VLTVG_VD_Enhance import build_vgmodel
-# from .VLTVG_VD_Enhance_ViT import build_vgmodel #这个是论文正式使用的model，切记
-# from .VLTVG_VD_Enhance_ViT_ablation import build_vgmodel
-
 from .transcp import build_vgmodel #这个是论文重构的model，精度对齐中
 
 def build_model(args):
